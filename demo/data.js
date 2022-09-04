f37fe35842e8956f3c38c7e565ac78241af07f0e
Lykked <lucas.osilva@sptech.school>
2022-09-04 03:44:34 -0300

    Heat Responsivo

--- demo/data.js
+++ demo/data.js
@@ -3,16 +3,16 @@ var data2 = [];
 
 //segundo//
 for (var i = 0; i < 300; ++i) {
-    data2.push([Math.random() * 100 + i / 8 + 330, 
-        Math.random() * 90 - i / 50 + 100, Math.random()]);
+    data2.push([Math.random() * 100 + i / 8 + 150, 
+        Math.random() * 90 - i / 30 + 115, Math.random()]);
 }
 for (var i = 0; i < 400; ++i) {
-    data2.push([Math.random() * 100 + i / 8 + 330, 
-        Math.random() * 90 - i / 50 + 100, Math.random()]);
+    data2.push([Math.random() * 100 + i / 8 + 150, 
+        Math.random() * 90 - i / 30 + 115, Math.random()]);
 }
 for (var i = 0; i < 100; ++i) {
-    data2.push([Math.random() * 100 + i / 8 + 330, 
-        Math.random() * 90 - i / 50 + 100, Math.random()]);
+    data2.push([Math.random() * 100 + i / 8 + 150, 
+        Math.random() * 90 - i / 30 + 115, Math.random()]);
 }
 
 
@@ -21,15 +21,15 @@ for (var i = 0; i < 100; ++i) {
 
 
 for (var i = 0; i < 600; ++i) {
-    data2.push([Math.random() * 200 + i / 8 + 220, 
-        Math.random() * 50 - i / 25 + 280, Math.random()]);
+    data2.push([Math.random() * 200 + i / 8 + 20, 
+        Math.random() * 50 - i / 25 + 310, Math.random()]);
 }
 for (var i = 0; i < 600; ++i) {
-    data2.push([Math.random() * 200 + i / 8 + 220, 
-        Math.random() * 50 - i / 25 + 280, Math.random()]);
+    data2.push([Math.random() * 200 + i / 8 + 20, 
+        Math.random() * 50 - i / 25 + 310, Math.random()]);
 }
 
 for (var i = 0; i < 100; ++i) {
-    data2.push([Math.random() * 200 + i / 8 + 220, 
-        Math.random() * 50 - i / 25 + 280, Math.random()]);
+    data2.push([Math.random() * 200 + i / 8 + 20, 
+        Math.random() * 50 - i / 25 + 310, Math.random()]);
 }
