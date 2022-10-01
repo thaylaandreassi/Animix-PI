0db393eb5274303d1ed7d20cc1583a28980992d6
alecostx <alexandre.rodrigues@sptech.school>
2022-10-01 19:35:34 -0300

    cadastro de maquinas

--- site/src/database/script.sql
+++ site/src/database/script.sql
@@ -1,8 +1,57 @@
-create database Animix;
-use Animix;
+create database animix;
 
-create table cadastro (
-usuario varchar(100),
-email varchar(100),
-senha varchar(100)
+use animix;
+
+
+create table studio(
+	idStudio int primary key auto_increment,
+    nomeEmpresa varchar(45),
+    email varchar(45),
+    senha varchar(45),
+    logradouro varchar(45),
+    telefone varchar(45),
+    CNPJ char(45)
+);
+
+create table funcionario(
+	idFuncionario int primary key auto_increment,
+    cargo varchar(45),
+    nome varchar(45),
+    email varchar(45),
+    senha varchar(45),
+    fkStudio int,
+    fkCargo int,
+    foreign key(fkStudio) references studio(idStudio),
+    foreign key(fkCargo) references funcionario(idFuncionario)
+);
+
+create table maquina (
+	idComputador int primary key auto_increment,
+    disco varchar(45),
+    discoIdeal double(10,2),
+    memoria varchar(45),
+    porcentMemoriaIdeal double(10,2),
+    processador varchar(45),
+    processamentIdeal double(10,2),
+    sistema varchar(45),
+    monitoraDisco boolean,
+    monitoraMemoria boolean,
+    monitoraProcessador boolean,
+    monitoraSistema boolean,
+    temperaturaIdeal boolean,
+    fkStudioMaq int,
+    fkCompMaster int,
+    foreign key(fkCompMaster) references maquina(idComputador),
+    foreign key(fkStudioMaq) references studio(idStudio)
+);
+
+create table dados (
+	idDados int primary key auto_increment,
+    temperatura double(10,2),
+    porcentCPU double(10,2),
+    porcentDisco double(10,2),
+    tempAtividade datetime,
+    qtdProcessadoresAtuantes int,
+    fkMaqDados int,
+    foreign key(fkMaqDados) references maquina(idComputador)
 );
---
+++ site/src/routes/maquinas.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+
+var maquinaController = require("../controllers/maquinaController");
+
+router.get("/", function (req, res) {
+    maquinaController.testar(req, res);
+});
+
+router.get("/listar", function (req, res) {
+    maquinaController.listar(req, res);
+});
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    maquinaController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    maquinaController.entrar(req, res);
+});
+
+module.exports = router;
