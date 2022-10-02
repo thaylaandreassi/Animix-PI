82c66343d1d927506939ba5170fd9f86e02098cf
alecostx <alexandre.rodrigues@sptech.school>
2022-10-02 20:29:13 -0300

    ajuste na query

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -5,10 +5,10 @@ function buscarMedidasEmTempoReal() {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select count(idUsuario) as qtd from cadastro;`;
+        instrucaoSql = `select count(idStudio) as qtd from studio; `;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select count(idUsuario) as qtd from cadastro;`;
+        instrucaoSql = `select count(idStudio) as qtd from studio; `;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
