edecf6d200390cfda49f54fbdec2adf6ded5c8f1
Alexandre Costa Rodrigues <99742799+alecostx@users.noreply.github.com>
2022-10-26 21:05:01 -0300

    Add files via upload

--- animix.sh
+++ animix.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-# sudo apt upgrade && sudo apt update 
-# java -version
-# if [ $? -eq 0 ];
-# then
-# echo "java instalado"
-# else
-# echo "java nao instalado"
-# echo "gostaria de instalar o java? (s/n)"
-# read inst
-# if [ \"$inst\" == \"s\" ];
-# then
-# sudo apt install default-jre -y
-# fi
-# fi
 sudo apt install docker.io 
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -25,5 +10,5 @@ sudo docker ps -a
 cd /home/ubuntu/Desktop
 git clone https://github.com/thaylaandreassi/Animix-PI.git
 
-cd Animix-PI/APPJar/target
+cd /home/ubuntu/Desktop/Animix-PI/APPJar/target
 java -jar banco-modelo-1.0-SNAPSHOT-jar-with-dependencies.jar
